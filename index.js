69e4f67a6068d78dd0ef9e5f4677e6b8da529f7a
dummyandriod18 <anup.2009.anup@gmail.com>
2019-03-22 19:13:35 +0530

    get title and rating for movie

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
 
+// movie - The Silence of the Lambs
 const URL = 'https://www.imdb.com/title/tt0102926/?ref_=nv_sr_1';
 
 (async () => {
   const response = await request(URL);
-  console.log(response);
+  let $ = cheerio.load(response);
+  // get title from h1
+  let title = $('div[class="title_wrapper"] > h1').text();
+  // excepted output - The Silence of the Lambs (1991)
+  let rating = $('span[itemprop="ratingValue"]').text();
+  console.log(title, rating);
 })();
