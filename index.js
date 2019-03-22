ce12fbfacf39d13117de1cf7a8f5b8cd0a01fa8d
anupmannem <anupmannem@gmail.com>
2019-03-22 23:27:30 +0530

    promisefying the call back

--- index.js
+++ index.js
@@ -87,35 +87,46 @@ const URLS = [
     // to download any file, first create a file stream
     let file = fs.createWriteStream(`${movie.id}.jpg`);
     // making request for the poster
-    let stream = request({
-      uri: poster,
-      header: {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'en-US,en;q=0.9',
-        'cache-control': 'max-age=0',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36',
-      },
-      gzip: true,
+    // using promises
+    await new Promise((resolve, reject) => {
+      let stream = request({
+        uri: poster,
+        header: {
+          'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+          'accept-encoding': 'gzip, deflate, br',
+          'accept-language': 'en-US,en;q=0.9',
+          'cache-control': 'max-age=0',
+          'upgrade-insecure-requests': '1',
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36',
+        },
+        gzip: true,
+      })
+        .pipe(file)
+        .on('finish', () => { // finish listener event to notify when download finishes
+          console.log(`finished downloading the image ${movie.id}`);
+          resolve();
+        })
+        .on('error', () => { // error listener event to reject the promise and throw an error
+          reject(error);
+        });
     })
-      .pipe(file);
+      // catch the error thrown by rejecting the promise
+      .catch((error) => console.log(`error while downloading ${movie.id} - ${error}`));
+    // write the movies data to a JSON file
+    // sync will tell node to wait for this
+    // to finish first, before moving ahead
+    // fs.writeFileSync('./data.json', JSON.stringify(moviesData), 'utf-8');
+
+    // writing to CSV format
+    // pick the fields to write to csv file
+    // if you choose to get all fields, then this fields is not needed
+    // const fields = ['title', 'rating'];
+    // pass the fields to the json2csvParser
+    // and parse the moviesData for those fields
+    // const json2csvParser = new Json2csvParser({ fields });
+    // const csv = json2csvParser.parse(moviesData);
+    // write to csv file
+    // fs.writeFileSync('./data.csv', csv, 'utf-8');
+    // console.log(csv);
   }
-
-  // write the movies data to a JSON file
-  // sync will tell node to wait for this
-  // to finish first, before moving ahead
-  // fs.writeFileSync('./data.json', JSON.stringify(moviesData), 'utf-8');
-
-  // writing to CSV format
-  // pick the fields to write to csv file
-  // if you choose to get all fields, then this fields is not needed
-  // const fields = ['title', 'rating'];
-  // pass the fields to the json2csvParser
-  // and parse the moviesData for those fields
-  // const json2csvParser = new Json2csvParser({ fields });
-  // const csv = json2csvParser.parse(moviesData);
-  // write to csv file
-  // fs.writeFileSync('./data.csv', csv, 'utf-8');
-  // console.log(csv);
 })();
